cbea29995b1c0c782277e8c26ba63ade4b5f669a
julie <107113672+Juliaamrl@users.noreply.github.com>
2024-09-05 20:12:18 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,153 @@
+export const perguntas = [
+    {
+        enunciado: "1. Qual seria o seu passatempo favorito?",
+        alternativas: [
+            {
+                texto: "Estudar magia e participar de feitiçarias!",
+                afirmacao: [
+                    "Você viveria no universo de Harry Potter, explorando o mundo da magia,"
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Explorar galáxias e lutar contra o lado sombrio",
+                afirmacao: [
+                    "Você se encaixa no universo de Star Wars, navegando por uma galáxia cheia de tecnologia futurista."
+                    
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "2. Qual é o seu tipo de herói ideal?",
+        alternativas: [
+            {
+                texto: "Um mago ou bruxa com poderes encantadores.",
+                afirmacao: [
+                    "Você pertence ao universo de Senhor dos Anéis, embarcando em jornadas heroicas."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Um Jedi ou piloto habilidoso com um sabre de luz.",
+                afirmacao: [
+                    "Você viveria no universo de Game of Thrones, manobrando intrigas políticas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "3. O que você valoriza mais em uma equipe?",
+        alternativas: [
+            {
+                texto: "Lealdade e habilidades mágicas únicas.",
+                afirmacao: [
+                    "estudando feitiçarias e vivendo aventuras em uma escola encantadora."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Coragem, habilidades de combate e lealdade",
+                afirmacao: [
+                    "poderes Jedi e aventuras intergalácticas."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "4. Qual é o seu item favorito para uma aventura?",
+        alternativas: [
+            {
+                texto: "Uma varinha mágica ou um grimório de feitiços.",
+                afirmacao: [
+                    "enfrentando criaturas míticas e lutando contra forças das trevas em uma terra lendária."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Um sabre de luz ou uma nave espacial avançada.",
+                afirmacao: [
+                    "sobrevivendo em um mundo medieval complexo e desafiador."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "5. Qual é o seu cenário ideal para uma aventura?",
+        alternativas: [
+            {
+                texto: "Uma escola de magia cheia de mistérios e feitiçaria.",
+                afirmacao: [
+                    "Personagens Harry Potter: Harry, Hermione, Luna, Ronny, Draco Malfoy ."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Uma galáxia distante com planetas exóticos e aventuras espaciais.",
+                afirmacao: [
+                    "Personagens de Star Wars: Yoda, Luke, Darth Vaider, Obi wan Kenobi."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "6. Como você lida com conflitos?",
+        alternativas: [
+            {
+                texto: "Usando inteligência mágica e estratégia.",
+                afirmacao: [
+                    "Personagens Game of thrones: Tyrion Lannister"
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Com coragem, combate e habilidades especiais.",
+                afirmacao: [
+                    "Personagem senhor dos anéis: Gandalf."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "7. Qual seria o seu papel em um grupo de aventureiros?",
+        alternativas: [
+            {
+                texto: "O mago ou estudioso, utilizando feitiçaria para ajudar a equipe.",
+                afirmacao: [
+                    ""
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "O combatente ou piloto, liderando e lutando com habilidades excepcionais.",
+                afirmacao: [
+                    "."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "8. Se você encontrasse um mapa antigo, o que esperaria encontrar?",
+        alternativas: [
+            {
+                texto: "Segredos mágicos e lugares escondidos de poder.",
+                afirmacao: [
+                    ""
+                ],
+            },
+            {
+                texto: "Rotas para planetas desconhecidos e aventuras intergalácticas.",
+                afirmacao: [
+                    ""
+                ],
+            },
+        ]
+    }
+];
